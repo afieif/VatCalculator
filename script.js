e8feb970c0ea56c4cb1c466c6e740fb51e1420ad
LAPTOP-0BE72HIK\afifk <afifkhanab@gmail.com>
2022-06-26 21:53:31 +0300

    size added

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 const rate = 15;
-let x;
+let res = 1;
+let x = 0;
+let h = 1;
+let l = 1;
 
-function addVat()
+function calc()
 {
-    const res = Number(x) + (x * rate / 100);
-    ele = document.getElementById('add');
-    ele.textContent = res;
-}
-
-function remVat()
-{
-    const res = (x * 100 / (100+rate)).toFixed(2);
-    ele = document.getElementById('rem');
-    ele.textContent = res;
-}
-
-function calc(){
+    h = document.getElementById('h').value;
+    w = document.getElementById('l').value;
     x = document.getElementById('base').value;
-    addVat();
-    remVat();
+
+    let withoutVat = ((+x)*(+h)*(+l)).toFixed(2);
+    let withVat = (withoutVat*115/100).toFixed(2);
+
+    ele = document.getElementById('woVat');
+    ele.textContent = withoutVat;
+
+    addEle = document.getElementById('wVat');
+    addEle.textContent = withVat;
 }
 
